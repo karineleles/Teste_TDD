4a138f5a133960d32c5dc2654de95468bafda74b
Karine <karine.leles@hotmail.com>
2012-11-19 00:53:00 -0200

    Codigo de Teste

---
+++ TDD/Tentativa/EnvioEmail.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Net.Mail;
+using System.Globalization;
+
+namespace TDD
+{
+    class EnvioEmail
+    {
+        public bool Envia(string endereco, string comentario)
+        {
+            MailMessage Email = new MailMessage();
+
+            Email.To.Add(endereco);
+            Email.From = new MailAddress("crmonlinesi@gmail.com", "CRM_Online", System.Text.Encoding.UTF8);
+            Email.Subject = "Lembrete Atividade";
+            Email.SubjectEncoding = System.Text.Encoding.UTF8;
+            Email.Body = "Aviso de lembrete: " + comentario + " no dia: " + DateTime.Now.Day + ", as " + DateTime.Now.Hour + ":" + DateTime.Now.Minute + "";
+            Email.SubjectEncoding = System.Text.Encoding.UTF8;
+            Email.IsBodyHtml = true;
+            Email.Priority = MailPriority.High;
+
+            SmtpClient cliente = new SmtpClient();
+
+            cliente.Credentials = new System.Net.NetworkCredential("crmonlinesi@gmail.com", "crm123456");
+            cliente.Port = 587;
+            cliente.Host = "smtp.gmail.com";
+            cliente.EnableSsl = true;
+
+            try
+            {
+
+                cliente.Send(Email);
+                return true;
+
+
+            }
+            catch (Exception ex)
+            {
+
+                return false;
+
+            }
+
+        }
+        public bool ValidaEmail(string email)
+        {
+            Regex rg = new Regex(@"^[A-Za-z0-9](([_\.\-]?[a-zA-Z0-9]+)*)@([A-Za-z0-9]+)(([\.\-]?[a-zA-Z0-9]+)*)\.([A-Za-z]{2,})$");
+
+            if (rg.IsMatch(email))
+            {
+                Console.Write("Email Valido!");
+                return true;
+            }
+            else
+            {
+                Console.Write("Email Inválido!");
+                return false;
+            }
+        }
+            public bool ValidacaoHora(string hora)
+            {
+                Regex rg = new Regex(@"^([0-9]{2}):([0-9]{2}):([0-9]{2})$");
+                string[] Vethora = hora.Split(char.Parse(":"));
+
+                if (hora == null && rg.IsMatch(hora) && int.Parse(Vethora[0]) > 24 && int.Parse(Vethora[1]) > 59 && int.Parse(Vethora[0]) < 0 && int.Parse(Vethora[1]) < 0)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            public string VerificaSemana(DateTime data)
+            {
+                CultureInfo culture = new CultureInfo("pt-BR");
+                DateTimeFormatInfo dtfi = culture.DateTimeFormat;
+                string data1 = dtfi.GetDayName(data.DayOfWeek);
+                return data1;
+            }
+            public bool EnvioNDias(DateTime data, string email, string comentario, int dias)
+            {
+                DateTime dataEnvio = data.AddDays(dias);
+
+                if (dataEnvio.DayOfWeek == DayOfWeek.Saturday || dataEnvio.DayOfWeek == DayOfWeek.Sunday)
+                {
+                    return false;
+                }
+                else
+                {
+                    if (DateTime.Compare(data, DateTime.Now) == -1)
+                    {
+                        Envia(email, comentario);
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+            public bool EnvioNDiasSabDom(DateTime data, string email, string comentario, int dias)
+            {
+                if (DateTime.Compare(data.AddDays(dias), DateTime.Now) == -1)
+                {
+                    Envia(email, comentario);
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            public bool ValidaData(DateTime data)
+            {
+                if (data < DateTime.Now)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+    }
+}
---
+++ TDD/TesteEmail/EnvioEmailTest.cs
@@ -0,0 +1,100 @@
+﻿using TDD;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TesteEmail
+{
+    
+    
+    [TestClass()]
+    public class EnvioEmailTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        
+
+
+        
+              
+        [TestMethod()]
+        public void EnviaTest()
+        {
+            EnvioEmail teste = new EnvioEmail(); 
+            string endereco = "karine.leles@hotmail.com";
+            string comentario = "Teste Email";
+            bool EmailTeste = teste.Envia(endereco, comentario);
+            Assert.AreEqual(true, EmailTeste);
+        }
+        [TestMethod()]
+        public void ValidaEmailTest()
+        {
+            EnvioEmail teste = new EnvioEmail();
+            string email = "teste.com";
+            bool ValidaEmail = teste.ValidaEmail(email);
+            Assert.AreEqual(true, ValidaEmail);   
+        }
+        [TestMethod()]
+        public void ValidacaoHoraTest()
+        {
+            EnvioEmail teste = new EnvioEmail(); 
+            string hora = "9:00";
+            bool horaValida= teste.ValidacaoHora(hora);
+            Assert.AreEqual(true, horaValida);
+        }
+        [TestMethod()]
+        public void VerificaSemanaTest()
+        {
+            EnvioEmail teste = new EnvioEmail();
+            DateTime data = new DateTime(2012,11,15); 
+            string semana = teste.VerificaSemana(data);
+            Assert.AreEqual("quinta-feira",semana );
+        }
+        [TestMethod()]
+        public void EnvioNDiasTest()
+        {
+            EnvioEmail teste = new EnvioEmail();
+            DateTime data = new DateTime(2012,11,15);
+            string email = "karine.leles@hotmail.com";
+            string comentario = "Email teste";
+            int dias = 3;
+            bool envio = teste.EnvioNDias(data, email, comentario, dias);
+            Assert.AreEqual(true, envio);
+        }
+        [TestMethod()]
+        public void EnvioNDiasSabDomTest()
+        {
+            EnvioEmail teste = new EnvioEmail();
+            DateTime data = new DateTime(2012,11,15); 
+            string email = "karine.leles@hotmail.com";
+            string comentario = "Email teste";
+            int dias = 3;
+            bool envio = teste.EnvioNDiasSabDom(data, email, comentario, dias);
+            Assert.AreEqual(true, envio);
+        }
+        [TestMethod()]
+        public void ValidaDataTest()
+        {
+            EnvioEmail teste = new EnvioEmail();
+            DateTime data = new DateTime(2012, 11, 21);
+            bool dataValida = teste.ValidaData(data);
+            Assert.AreEqual(true, dataValida);
+        }
+
+        
+        
+    }
+}
